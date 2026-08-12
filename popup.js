fda81bb54efb0f48e45c2ce97b285ec370208cc7
Samo-samo <Samo-samo@users.noreply.github.com>
2026-08-12 03:59:18 +0300

    Improve the UI

--- popup.js
+++ popup.js
@@ -4,34 +4,55 @@ let selectedLang = "";
 
 lang(selectedLang);
 
-const homepage = document.getElementById('homepage');
-const settingsPage = document.getElementById('settingsPage');
-const addingPage = document.getElementById('addingPage');
+function switchPage(activePageId) {
+    document.getElementById('homepage').style.display = 'none';
+    document.getElementById('settingsPage').style.display = 'none';
+    document.getElementById('addingPage').style.display = 'none';
+    
+    const activePage = document.getElementById(activePageId);
+    activePage.style.display = 'block';
+    
+    activePage.style.animation = 'none';
+    activePage.offsetHeight;
+    activePage.style.animation = null;
+}
 
 document.getElementById('settingsBtn').addEventListener('click', () => {
-    homepage.style.display = 'none';
-    settingsPage.style.display = 'block';
-    
-    const selectedLang = getLang();
-    document.getElementById('lang').value = selectedLang;
+    switchPage('settingsPage');
 });
 
 document.getElementById('addingPageBtn').addEventListener('click', () => {
-    homepage.style.display = 'none';
-    addingPage.style.display = 'block';
+    switchPage('addingPage');
 });
 
 document.getElementById('backBtnSettings').addEventListener('click', () => {
-    settingsPage.style.display = 'none';
-    homepage.style.display = 'block';
+    switchPage('homepage');
 });
 
 document.getElementById('backBtnAdd').addEventListener('click', () => {
-    addingPage.style.display = 'none';
-    homepage.style.display = 'block';
+    switchPage('homepage');
 });
 
 document.getElementById('lang').addEventListener('change', function(event) {
     const selectedLang = event.target.value;
     lang(selectedLang);
 })
+
+const darkModeToggle = document.getElementById('darkMode');
+
+// Eğer Chrome Extension mimarisindeyse localStorage yerine chrome.storage.local da kullanılabilir
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme === 'dark') {
+    document.body.classList.add('dark');
+    darkModeToggle.checked = true;
+}
+
+darkModeToggle.addEventListener('change', () => {
+    if (darkModeToggle.checked) {
+        document.body.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        document.body.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
+    }
+});
